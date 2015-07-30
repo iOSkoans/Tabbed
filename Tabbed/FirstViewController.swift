cc795222db3e6265af069cd4f2929235aedbeda6
Calvin Cheng <linchuan.cheng@gmail.com>
2015-07-30 12:50:54 +0800

    Initial Commit

---
+++ Tabbed/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbed
+//
+//  Created by Calvin Cheng on 30/7/15.
+//  Copyright (c) 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tabbed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tabbed
+//
+//  Created by Calvin Cheng on 30/7/15.
+//  Copyright (c) 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
